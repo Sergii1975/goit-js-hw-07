8e0f9d078e396b309bc50753241a01535714dc83
Sergii <svbulgakov75@gmail.com>
2023-05-03 16:52:21 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,9 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const list = document.querySelector('ul.gallery');
+const markup = galleryItems.map(({preview, original, description }) => 
+    `<li><img src='${original}' alt='${description}'> </li>`).join('');
+list.insertAdjacentHTML('afterend', markup);
+
 
 console.log(galleryItems);
