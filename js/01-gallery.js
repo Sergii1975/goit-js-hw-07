670727dabec7364ab6a55036c03c454cd54c0fe9
Sergii <svbulgakov75@gmail.com>
2023-05-06 20:57:53 +0300

    AllUpdate

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,9 +1,44 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const list = document.querySelector('ul.gallery');
-const markup = galleryItems.map(({preview, original, description }) => 
-    `<li><img src='${original}' alt='${description}'> </li>`).join('');
-list.insertAdjacentHTML('afterend', markup);
 
+const markup = galleryItems.map(({ preview, original, description }) =>
+    `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+  <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+    </li>
+    `).join('');
+
+list.insertAdjacentHTML('afterbegin', markup);
+    
+list.addEventListener('click', onCardClick);
+
+function onCardClick(event) {
+  event.preventDefault()
+  const selectedImg = event.target.dataset.source
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  const instance = basicLightbox.create(`
+    <img src="${selectedImg}"/>
+`)
+instance.show()
+
+document.addEventListener('keydown', function(event) {
+if (event.key === 'Escape') {
+instance.close()
+}
+});
+}
 
 console.log(galleryItems);
+
+
+
+    
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const list = document.querySelector('ul.gallery');
 
+const markup = galleryItems.map(({ preview, original, description }) =>
+    `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>
+    `).join('');
+
+list.insertAdjacentHTML('afterbegin', markup);
+
+list.addEventListener('click', onCardClick);
+
+function onCardClick(event) {
+  event.preventDefault()
+  const selectedImg = event.target.dataset.source
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+      
+}
+new SimpleLightbox('.gallery a', { captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250 });
 console.log(galleryItems);
